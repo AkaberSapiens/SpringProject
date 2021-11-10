53ef00a4c3e51dfc6945279c43c822b890816dbc
AkaberSapiens <Akaber.Zalaf@sapiens.com>
2021-11-10 06:48:52 +0200

    JPA

---
+++ springDemo/src/main/java/com/sapiens/springDemo/SpringJPA/Dao.java
@@ -0,0 +1,16 @@
+package com.sapiens.springDemo.SpringJPA;
+
+import java.util.List;
+
+public interface Dao {
+
+    void insertContact(Contact con);
+    List<Contact> findAll();
+    Contact findByContactId(int contact_Id);
+    List<Contact> findByCountry(String country);
+    List<Contact> findByPremiumAmount(int premium);
+    List<Contact> findByInsuranceType(String type);
+    void update(Contact contact);
+    void delete(int i);
+
+}
